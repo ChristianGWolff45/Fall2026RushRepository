54aca334907354b2c35526ea4f9840646a344301
Christian Wolff <christiangwolff@gmail.com>
2026-09-10 19:37:18 -0400

    updated name

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,12 +1,9 @@
 export function Hero() {
   return (
-    <section
-      id="top"
-      className="flex flex-col gap-8 pt-2"
-    >
+    <section id="top" className="flex flex-col gap-8 pt-2">
       <div className="flex flex-col gap-2">
         <h1 className="font-mono text-4xl font-semibold tracking-tight text-text-primary sm:text-5xl lg:text-6xl">
-          Firstname Lastname
+          Christian Wolff
         </h1>
         <h2 className="font-mono text-3xl font-semibold text-secondary sm:text-5xl">
           Computer Science
